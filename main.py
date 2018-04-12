c00a52466933275e606c6783181d922fb43f9a4e
Ahmed Zaima <a_zaima@yahoo.com>
2018-04-12 18:35:54 +0100

    second commit

---
+++ main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import os
+import jinja2
+
+template_dir = os.path.dirname(__file__)
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+caps2 = 'NOPQRSTUVWXYZABCDEFGHIJKLM'
+regs = caps.lower()
+regs2 = caps2.lower()
+
+class Handler(webapp2.RequestHandler):
+    
+    def write(self, *a , **kw):
+        self.response.out.write(*a, **kw)
+    
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class MainHandler(Handler):
+    def get(self):
+        self.render("main.html")
+    
+    def post(self):
+        input = self.request.get("input")
+        newInput = ""
+        for chr in input:
+            if caps.find(chr) > -1:
+                pos = caps.find(chr)
+                newInput += caps2[pos]
+
+            elif regs.find(chr) > -1:
+                pos = regs.find(chr)
+                newInput += regs2[pos]
+
+            else:
+                newInput += chr
+
+
+        self.render("main.html", input = newInput)
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
